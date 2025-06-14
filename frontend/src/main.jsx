4a9f18b37e4b78f73a58095bb4e941b34d627c4d
Nhan Chau <chauhieunhan01@gmail.com>
2025-06-14 14:46:12 +0700

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,10 +8,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
