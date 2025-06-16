6616f4d0c0be056f3e5dab0f689a5e03a5addef9
Nhan Chau <chauhieunhan01@gmail.com>
2025-06-16 10:34:23 +0700

    .

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33727fa37e6048bd8e9db853f00852c09146e5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54eb1ce078c2d4128fd1dbed63675773ad5e8b0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
