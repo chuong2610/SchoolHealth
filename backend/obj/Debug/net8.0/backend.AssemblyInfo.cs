74f4946258b50b695dd57a85cb504afd487303db
chuong2610 <167516421+chuong2610@users.noreply.github.com>
2025-06-26 20:02:35 +0700

    fix chat

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04701380b0329c6c4566eed121436c51cb34b2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd024ecac1791e9706794c82150235c5507c81f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
